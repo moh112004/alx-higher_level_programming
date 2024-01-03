117598d15361bc557b1a923e5c3334cda7d9908f
moh112004 <almstfym870@gmail.com>
2024-01-03 08:09:08 +0300

    files

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @a: An integer
+ * @b: Points to kthe next node
+ *
+ */
+typedef struct listint_s
+{
+	int a;
+	struct listint_s *b;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int a);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
