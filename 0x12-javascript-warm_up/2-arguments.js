76f912c228b7c45926fc700143d9efc0f8b4573e
moh112004 <almstfym870@gmail.com>
2024-05-27 14:40:11 +0300

    upload

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-//Check number of arguments passed
+// Calculate number of arguments passed
 
 if (process.argv.length === 2) {
-   console.log('No argument');
-}
-else if (process.argv.length === 3) {
-     console.log('Argument found');
-}
-else {
-       console.log('Arguments found');
-}
+  console.log('No argument');
+  } else if (process.argv.length === 3) {
+    console.log('Argument found');
+    } else {
+      console.log('Arguments found');
+      }
